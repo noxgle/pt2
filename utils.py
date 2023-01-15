37e7603ff789e909148b538980d04b9200e74dad
noxgle <s.wielgosz@noxgle.com>
2023-01-15 17:25:35 +0100

    Small fix.

--- utils.py
+++ utils.py
@@ -12,11 +12,12 @@ def load_conf(section):
     try:
         cf = configparser.ConfigParser()
         cf.read_file(open('conf'))
-        conf =dict(cf[section])
+        conf = dict(cf[section])
     except Exception as e:
         print(e)
         sys.exit()
     return conf
+
 
 def resize_img(img, size, interpolation):
     '''
@@ -42,22 +43,25 @@ def resize_img(img, size, interpolation):
         mask[y_pos:y_pos + h, x_pos:x_pos + w, :] = img[:h, :w, :]
     return cv2.resize(mask, (size, size), interpolation), {'x_pos': x_pos, 'y_pos': y_pos, 'ratio': dif / size}
 
-def set_logging(level='error',to_file=False):
-    if level=='debug':
-        set_level=logging.DEBUG
-    elif level=='info':
-        set_level=logging.INFO
-    elif level=='warn':
-        set_level=logging.WARN
-    elif level=='error':
-        set_level=logging.ERROR
-    elif level=='critical':
-        set_level=logging.CRITICAL
+
+def set_logging(level='error', to_file=False):
+    if level == 'debug':
+        set_level = logging.DEBUG
+    elif level == 'info':
+        set_level = logging.INFO
+    elif level == 'warn':
+        set_level = logging.WARN
+    elif level == 'error':
+        set_level = logging.ERROR
+    elif level == 'critical':
+        set_level = logging.CRITICAL
+    else:
+        set_level = logging.INFO
 
     if to_file is True:
-        handler=logging.FileHandler("debug.log")
+        handler = logging.FileHandler("debug.log")
     else:
-        handler=logging.StreamHandler()
+        handler = logging.StreamHandler()
 
     logging.basicConfig(
         level=set_level,
